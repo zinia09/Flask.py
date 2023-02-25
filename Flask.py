936f1da24eb8eb9022d04f306fe42c8801d28205
zinia09 <110440881+zinia09@users.noreply.github.com>
2023-02-25 16:58:33 -0500

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -1,16 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/')
 def index():
-    return "Hello from Pet Adoption"
+    return "Homepage"
 
-@app.route('/cats')
-def cats():
-    return "Cats"
+@app.route('/projects/new')
+def new():
+    return
+
+@app.route('/projects/<id>')
+def id():
+    return
+
+@app.route('/projects/<id>/edit')
+def edit():
+    return
+
+@app.route('/projects/<id>/delete')
+def delete():
+    return
 
 
 
