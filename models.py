22d4a0e61a152997db543aa76e9f82c2886820ed
zinia09 <110440881+zinia09@users.noreply.github.com>
2023-03-24 15:10:18 -0400

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
 db = SQLAlchemy(app)
